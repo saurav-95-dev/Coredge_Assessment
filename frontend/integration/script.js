474dc03220559813adf342d66dc0dc8f810e56e0
UserShub <shubhankardey29sep@gmail.com>
2024-02-04 14:56:46 +0530

    New Updated Changes

--- frontend/integration/script.js
+++ frontend/integration/script.js
@@ -43,6 +43,7 @@ async function startRecording() {
 
             audioChunks = [];
             videoChunks = [];
+            window.location.reload();
         };
 
         mediaRecorder.start();
